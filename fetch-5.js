2e189103f9a67ead848f0e3116eba8fe81d1f6b1
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-09-26 00:59:06 -0500

    manejo de respuestas y errores

--- fetch-5.js
+++ fetch-5.js
@@ -1,24 +1,17 @@
-fetch('https://reqres.in/api/users/1')
+fetch('https://reqres.in/api/users/1000')
     .then(resp => {
 
+        if (resp.ok) {
+            return resp.json();
 
-        resp.clone().json()
-            .then(usuario => {
-                console.log(usuario.data);
-            });
-
-        resp.clone().json()
-            .then(usuario => {
-                console.log(usuario.data);
-            });
-
-        resp.json()
-            .then(usuario => {
-                console.log(usuario.data);
-            });
+        } else {
+            throw new Error('No existe el usuario 1000')
+        }
 
 
-
-
+    })
+    .catch(error => {
+        console.log('Error en la petición');
+        console.log(error);
 
     });
